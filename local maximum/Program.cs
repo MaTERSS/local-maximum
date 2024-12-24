989c4ee9c876345c763fde75ef00f3ab8c1274a5
MaTERSS <146178004+MaTERSS@users.noreply.github.com>
2024-12-24 20:47:25 +0800

    Update Program.cs

--- local maximum/Program.cs
+++ local maximum/Program.cs
@@ -14,7 +14,7 @@ namespace CSharplight
     internal class Programm
     {
         static void Main(string[] args)
-        {            
+        {
             int elements = 30;
             int minValue = 0;
             int maxValue = 51;
@@ -23,39 +23,32 @@ namespace CSharplight
             Random random = new Random();
 
             Console.WriteLine("Исходный массив:");
-;
+
             for (int i = 0; i < numbers.Length; i++)
             {
-                numbers[i] = random.Next(minValue, maxValue); 
+                numbers[i] = random.Next(minValue, maxValue);
                 Console.Write(numbers[i] + " ");
             }
 
             Console.WriteLine();
-
             Console.WriteLine("Локальные максимумы:");
-            for (int i = 0; i < numbers.Length; i++)
-            {                
-                if (i == 0) 
+
+            if (numbers.Length > 1 && numbers[0] > numbers[1])
+            {
+                Console.WriteLine(numbers[0]);
+            }
+
+            for (int i = 1; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
                 {
-                    if (numbers[i] > numbers[i + 1])
-                    {
-                        Console.WriteLine(numbers[i]);
-                    }
+                    Console.WriteLine(numbers[i]);
                 }
-                else if (i == numbers.Length - 1) 
-                {
-                    if (numbers[i] > numbers[i - 1])
-                    {
-                        Console.WriteLine(numbers[i]);
-                    }
-                }
-                else 
-                {
-                    if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
-                    {
-                        Console.WriteLine(numbers[i]);
-                    }
-                }
+            }
+
+            if (numbers.Length > 1 && numbers[numbers.Length - 1] > numbers[(numbers.Length - 1)-1])
+            {   
+                Console.WriteLine(numbers[numbers.Length - 1]);
             }
         }
     }
